4163ad1ee422d8bc487dca35cdabf60cd5b47eee
Lyssa Utida <lyssamailbox@gmail.com>
2022-05-14 03:46:54 -0400

    feat: create test route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
+import routes from './routes';
 
 class App {
   app: Express;
@@ -8,12 +9,17 @@ class App {
   constructor() {
     this.app = express();
     this.middlewares();
+    this.routes();
   }
 
   middlewares() {
     this.app.use(cors());
     this.app.use(express.json());
     this.app.use(helmet());
+  }
+
+  routes() {
+    this.app.use(routes);
   }
 }
 
